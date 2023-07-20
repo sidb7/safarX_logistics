32306b8171009072b4151659ce4e21c910628c64
Jai <jaisarki123@gmail.com>
2023-07-20 19:41:16 +0530

    created right modal

---
+++ src/components/CustomModal/customRightModal.tsx
@@ -0,0 +1,26 @@
+import ReactModal from "react-modal";
+
+interface RightSideModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const RightSideModal: React.FC<RightSideModalProps> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="fixed  inset-y-0 right-0 flex flex-col justify-center items-center bg-white w-1/3"
+      overlayClassName="fixed z-20 inset-0 bg-gray-800 bg-opacity-50"
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default RightSideModal;
