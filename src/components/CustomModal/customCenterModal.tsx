3bfb039c733967f0c90e2173daea4f0e43218e21
Navin Bhatotra <navin.bhatotra@shipyaari.com>
2023-07-24 15:30:55 +0530

    Made walletRecharge

--- src/components/CustomModal/customCenterModal.tsx
+++ src/components/CustomModal/customCenterModal.tsx
@@ -4,18 +4,20 @@ interface CenterSideModalProps {
   isOpen: boolean;
   onClose: () => void;
   children?: React.ReactNode;
+  className?: string;
 }
 
 const CenterSideModal: React.FC<CenterSideModalProps> = ({
   isOpen,
   onClose,
   children,
+  className = "",
 }) => {
   return (
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onClose}
-      className="fixed inset-0 m-auto w-3/6 flex flex-col justify-center items-center rounded-lg overflow-hidden bg-white h-5/6 "
+      className={`${className} fixed inset-0 m-auto w-3/6 flex flex-col justify-center items-center rounded-lg overflow-hidden bg-white h-5/6`}
       overlayClassName="fixed z-20 inset-0 bg-gray-800 bg-opacity-50"
     >
       {children}
