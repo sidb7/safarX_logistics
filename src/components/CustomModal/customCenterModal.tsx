06dfd7bdaf502e49260be43059acc27dfe6c9b40
Navin Bhatotra <navin.bhatotra@shipyaari.com>
2023-07-24 13:09:25 +0530

    Login Screen made for Desktop

---
+++ src/components/CustomModal/customCenterModal.tsx
@@ -0,0 +1,26 @@
+import ReactModal from "react-modal";
+
+interface CenterSideModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const CenterSideModal: React.FC<CenterSideModalProps> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="fixed inset-0 m-auto w-3/6 flex flex-col justify-center items-center rounded-lg overflow-hidden bg-white h-5/6 "
+      overlayClassName="fixed z-20 inset-0 bg-gray-800 bg-opacity-50"
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default CenterSideModal;
