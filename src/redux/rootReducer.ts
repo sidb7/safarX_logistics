db553cfb941a25cd03f3e50221f2ef77d9834c69
suresh <suresh.parmar@shipyaari.com>
2023-07-27 13:01:39 +0530

    add new chnages

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./reducers/useReducer";
+import userReducer from "./reducers/userReducer";
 import mapReducer from "./reducers/mapReducer";
 import catalogueReducer  from "./reducers/catalogue";
 
