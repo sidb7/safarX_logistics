97368185b1cc8877a5414d0a5f7631ee83761cca
Mahavir-Shipyaari <mahavir.vaishnav@shipyaari.com>
2023-08-16 16:23:54 +0530

    {UPDATE, CREATE} Delete Modal Created, Profile Section UI Fixed

--- src/screens/Profile/Notification/notificationTab.tsx
+++ src/screens/Profile/Notification/notificationTab.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "react-responsive";
 import PersonIcon from "../../../assets/Profile/Notification/PersonIcon.svg";
-import ToggleIcon from "../../../assets/notifications/Property 1=toggle on.svg"
+import ToggleIcon from "../../../assets/notifications/Property 1=toggle on.svg";
 import CustomInputBox from "../../../components/Input";
 import { ToggleButton } from "../../../components/ToggleButton";
 export const ProfileNotificationTab = () => {
@@ -23,8 +23,14 @@ export const ProfileNotificationTab = () => {
           <div>
             <CustomInputBox label="Operational contact" />
           </div>
-          <br/>
-          <div className="flex justify-between items-center shadow-sm rounded-md px-2 h-[44px]">
+          <br />
+          <div
+            style={{
+              boxShadow:
+                "0px 0px 0px 0px rgba(133, 133, 133, 0.05), 0px 6px 13px 0px rgba(133, 133, 133, 0.05)",
+            }}
+            className="flex justify-between items-center rounded-md px-2 h-[44px]"
+          >
             <span className="whitespace-nowrap">Notification (Email, SMS)</span>
             <ToggleButton imgSrc={ToggleIcon} />
           </div>
@@ -33,7 +39,7 @@ export const ProfileNotificationTab = () => {
 
       <div className="flex flex-col mt-10">
         <div className="flex items-center">
-        {!isItLgScreen && <img src={PersonIcon} alt="personIcon" />}
+          {!isItLgScreen && <img src={PersonIcon} alt="personIcon" />}
           <span className="ml-2 text-[#323232] text-[12px] font-bold lg:text-[24px] lg:font-normal">
             Account Details
           </span>
@@ -45,21 +51,41 @@ export const ProfileNotificationTab = () => {
           <div>
             <CustomInputBox label="Account contact" />
           </div>
-          <br/>
-          <div className="flex justify-between items-center shadow-md rounded-md px-2 mt-4 h-[44px]">
-            <span className="flex whitespace-nowrap">Notification (Email, SMS)</span>
+          <br />
+          <div
+            style={{
+              boxShadow:
+                " 0px 0px 0px 0px rgba(133, 133, 133, 0.05), 0px 6px 13px 0px rgba(133, 133, 133, 0.05);",
+            }}
+            className="flex justify-between items-center  rounded-md px-2 mt-4 h-[44px]"
+          >
+            <span className="flex whitespace-nowrap">
+              Notification (Email, SMS)
+            </span>
             <ToggleButton imgSrc={ToggleIcon} />
           </div>
         </div>
       </div>
 
       <div className="lg:grid lg:grid-cols-3 gap-4 mt-10">
-        <div className="flex justify-between items-center shadow-md rounded-md px-2 h-[44px]">
+        <div
+          style={{
+            boxShadow:
+              "0px 0px 0px 0px rgba(133, 133, 133, 0.05), 0px 6px 13px 0px rgba(133, 133, 133, 0.05)",
+          }}
+          className="flex justify-between items-center rounded-md px-2 h-[44px]"
+        >
           <span className="flex whitespace-nowrap">Tracking notification</span>
           <ToggleButton imgSrc={ToggleIcon} />
         </div>
 
-        <div className="flex justify-between items-center shadow-md rounded-md px-2 h-[44px]">
+        <div
+          style={{
+            boxShadow:
+              "0px 0px 0px 0px rgba(133, 133, 133, 0.05), 0px 6px 13px 0px rgba(133, 133, 133, 0.05)",
+          }}
+          className="flex justify-between items-center shadow-md rounded-md px-2 h-[44px]"
+        >
           <span className="whitespace-nowrap">Rating email</span>
           <ToggleButton imgSrc={ToggleIcon} />
         </div>
