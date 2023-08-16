d2fb3aa024b4b0be49090138af3b2e2108076a42
Akshay Kumar Mehara <akshay.mehara@shipyaari.com>
2023-08-16 18:28:51 +0530

    Revert "add hoverside with url"

--- src/screens/Order/Service/index.tsx
+++ src/screens/Order/Service/index.tsx
@@ -8,74 +8,70 @@ import ServiceCard from "./ServiceCard";
 
 import { RecommendedServiceData } from "../../../utils/dummyData";
 import { FilterServiceData } from "../../../utils/dummyData";
-import Sidebar from "../../../layout/HoverSidebar";
 
 const Service: React.FC = () => {
   const items = ["Fastest", "Low Price", "Economy", "Standard", "Premium"];
 
   return (
-    <div className="flex">
-      {/* <Sidebar /> */}
-      <div>
-        <div className="flex flex-row gap-x-2 mb-5 lg:mb-[38px] mx-5">
-          <img src={TruckIcon} alt="Truck Icon" className="lg:hidden" />
-          <img src={WebTruckIcon} alt="" className="hidden lg:block" />
-          <p className="text-[24px] text-[#202427] font-semibold lg:font-normal lg:text-2xl lg:text-[#323232] ">
-            Service
-          </p>
-        </div>
-        <div className="flex flex-col lg:flex-row     gap-y-[22px] mx-5 mb-5 lg:gap-6 lg:mb-9">
-          {RecommendedServiceData.map((each) => {
-            return (
-              <ServiceCard
-                isRecommendation={each.isRecommendation}
-                recommendation={each.recommendation}
-                courierPartner={each.courierPartner}
-                serviceType={each.serviceType}
-                weight={each.weight}
-                totalPrice={each.totalPrice}
-                savePrice={each.savePrice}
-                etaDate={each.etaDate}
-                name={each.name}
-                value={each.value}
-              />
-            );
-          })}
-        </div>
+    <div>
+      <div className="flex flex-row gap-x-2 mb-5 lg:mb-[38px] mx-5">
+        <img src={TruckIcon} alt="Truck Icon" className="lg:hidden" />
+        <img src={WebTruckIcon} alt="" className="hidden lg:block" />
+        <p className="text-[24px] text-[#202427] font-semibold lg:font-normal lg:text-2xl lg:text-[#323232] ">
+          Service
+        </p>
+      </div>
+      <div className="flex flex-col lg:flex-row     gap-y-[22px] mx-5 mb-5 lg:gap-6 lg:mb-9">
+        {RecommendedServiceData.map((each) => {
+          return (
+            <ServiceCard
+              isRecommendation={each.isRecommendation}
+              recommendation={each.recommendation}
+              courierPartner={each.courierPartner}
+              serviceType={each.serviceType}
+              weight={each.weight}
+              totalPrice={each.totalPrice}
+              savePrice={each.savePrice}
+              etaDate={each.etaDate}
+              name={each.name}
+              value={each.value}
+            />
+          );
+        })}
+      </div>
 
-        <div className="mx-5  mb-5 lg:mb-6">
-          <div>
-            <div className="flex flex-row items-center gap-x-2 mb-5">
-              <img src={FilterIcon} alt="Filter" />
-              <div className="text-[24px] font-bold lg:font-normal lg:text-2xl">
-                Filter by
-              </div>
+      <div className="mx-5  mb-5 lg:mb-6">
+        <div>
+          <div className="flex flex-row items-center gap-x-2 mb-5">
+            <img src={FilterIcon} alt="Filter" />
+            <div className="text-[24px] font-bold lg:font-normal lg:text-2xl">
+              Filter by
             </div>
           </div>
-          <FilterItems
-            items={items}
-            onClick={(e) => console.log("e :", e.target.id)}
-          />
         </div>
+        <FilterItems
+          items={items}
+          onClick={(e) => console.log("e :", e.target.id)}
+        />
+      </div>
 
-        <div className="mx-5 flex flex-col lg:flex-row h-[500px] lg:h-full   gap-y-[13px] overflow-y-scroll  lg:overflow-x-scroll flex-nowrap   lg:gap-x-6  ">
-          {FilterServiceData.map((each) => {
-            return (
-              <ServiceCard
-                isRecommendation={each.isRecommendation}
-                recommendation={each.recommendation}
-                courierPartner={each.courierPartner}
-                serviceType={each.serviceType}
-                weight={each.weight}
-                totalPrice={each.totalPrice}
-                savePrice={each.savePrice}
-                etaDate={each.etaDate}
-                name={each.name}
-                value={each.value}
-              />
-            );
-          })}
-        </div>
+      <div className="mx-5 flex flex-col lg:flex-row h-[500px] lg:h-full   gap-y-[13px] overflow-y-scroll  lg:overflow-x-scroll flex-nowrap   lg:gap-x-6  ">
+        {FilterServiceData.map((each) => {
+          return (
+            <ServiceCard
+              isRecommendation={each.isRecommendation}
+              recommendation={each.recommendation}
+              courierPartner={each.courierPartner}
+              serviceType={each.serviceType}
+              weight={each.weight}
+              totalPrice={each.totalPrice}
+              savePrice={each.savePrice}
+              etaDate={each.etaDate}
+              name={each.name}
+              value={each.value}
+            />
+          );
+        })}
       </div>
     </div>
   );
