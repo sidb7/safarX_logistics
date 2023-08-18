01911bbd89e1d69c40e2c0415a7c420d88078191
keval-shipyaari <keval.panchal@shipyaari.com>
2023-08-18 19:46:16 +0530

    order list added ui

--- src/utils/ApiUrls.ts
+++ src/utils/ApiUrls.ts
@@ -6,7 +6,7 @@ Enviornment = process.env.REACT_APP_ENV || "development";
 
 switch (Enviornment) {
   case "development":
-    SELLER_URL = `${process.env.REACT_APP_SELLER_DEV}`;
+    SELLER_URL = `${process.env.REACT_APP_SELLER_LOCAL}`;
     FILE_SERVER_URL = `${process.env.REACT_APP_FILE_SERVER_DEV}`;
     break;
   case "production":
@@ -25,9 +25,11 @@ const POST_SEND_OTP_URL = `${SELLER_URL}/api/v1/seller/sendSignUpOtp`;
 const GET_PROFILE_URL = `${SELLER_URL}/api/v1/seller/getSingleSeller`;
 const GET_PROFILE_NOTIFICATION = `${SELLER_URL}/api/v1/notification/readNotification`;
 const UPDATE_PROFILE_NOTIFICATION = `${SELLER_URL}/api/v1/notification/updateNotification`;
+const GET_SELLER_ORDER = `${SELLER_URL}/api/v1/order/getSellerOrder`;
 
 export {
   SELLER_URL,
+  GET_SELLER_ORDER,
   POST_SIGN_IN_URL,
   POST_SIGN_UP_URL,
   POST_SEND_OTP_URL,
