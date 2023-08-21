635387ce6c3b2c4f74b731bef6fc1cc6d1074b12
Navin Bhatotra <navin.bhatotra@shipyaari.com>
2023-08-21 19:40:16 +0530

    Fixed bottom layout for lg screen

--- src/components/Layout/bottomLayout.tsx
+++ src/components/Layout/bottomLayout.tsx
@@ -8,7 +8,7 @@ const BottomLayout = ({ callApi }: any) => {
 
   return (
     <footer className="w-full fixed bottom-0">
-      <div className="grid grid-cols-2  shadow-lg border-[1px]  bg-[#FFFFFF] gap-[32px] p-[24px] rounded-tr-[24px] rounded-tl-[24px] fixed w-full bottom-0 lg:flex lg:justify-end">
+      <div className="grid grid-cols-2  shadow-lg border-[1px] bg-[#FFFFFF] gap-[32px] p-[24px] rounded-tr-[24px] rounded-tl-[24px] fixed w-full bottom-0 lg:flex lg:justify-end lg:!w-[calc(100%-64px)]">
         <ServiceButton
           className="lg:w-[100px]"
           text="BACK"
