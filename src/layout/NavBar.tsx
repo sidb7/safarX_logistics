7820a69de9a83c2736bca62c79b431c8db7d927c
Navin Bhatotra <navin.bhatotra@shipyaari.com>
2023-08-21 16:21:05 +0530

    Added Profile navigate

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -116,6 +116,7 @@ const NavBar = (props: IpropTypes) => {
                     <button
                       className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                       role="menuitem"
+                      onClick={() => navigate("/profile")}
                     >
                       Your Profile
                     </button>
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -721,6 +721,7 @@ const Layout = () => {
                           <button
                             className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                             role="menuitem"
+                            onClick={() => navigate("/profile")}
                           >
                             Your Profile
                           </button>
