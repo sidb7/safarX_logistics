5d5b8a39d34b75d96d3ead698b68e88c46c9e28d
Navin Bhatotra <navin.bhatotra@shipyaari.com>
2023-08-22 11:40:38 +0530

    Added breadcum in Refer and Earn

--- src/screens/Profile/ReferEarn/referTab.tsx
+++ src/screens/Profile/ReferEarn/referTab.tsx
@@ -4,38 +4,44 @@ import ShareIcon from "../../../assets/Label/share.svg";
 import CopyIcon from "../../../assets/Transaction/CopyIcon.svg";
 
 import CustomButton from "../../../components/Button";
+import BottomLayout from "../../../components/Layout/bottomLayout";
+import { Breadcum } from "../../../components/Layout/breadcum";
 export const ReferTab = () => {
   const isItLgScreen = useMediaQuery({
     query: "(min-width: 1024px)",
   });
   return (
-    <div className="mt-[77px]">
-      <div className="flex justify-center items-center">
-        <img src={ReferCode} alt="" />
-      </div>
-      <div className="flex justify-center mt-[48px] p-3 text-[16px] font-semibold whitespace-nowrap">
-        Referral Code - QYHR78171JEY
-      </div>
-      <div className="flex justify-center mt-[40px]">
-        <div className="p-2">
-          <CustomButton
-            text="SHARE"
-            showIcon={true}
-            icon={ShareIcon}
-            onClick={() => {}}
-            className="p-2 rounded !bg-[#F2F6FF] !text-[#0066FF]"
-          />
+    <div className="h-full">
+      <Breadcum label="Refer and Earn" />
+      <div className="mt-[77px]">
+        <div className="flex justify-center items-center">
+          <img src={ReferCode} alt="" />
         </div>
-        <div className="p-2">
-          <CustomButton
-            text="COPY"
-            showIcon={true}
-            icon={CopyIcon}
-            onClick={() => {}}
-            className="p-2 rounded !bg-[#F2F6FF] !text-[#0066FF]"
-          />
+        <div className="flex justify-center mt-[48px] p-3 text-[16px] font-semibold whitespace-nowrap">
+          Referral Code - QYHR78171JEY
+        </div>
+        <div className="flex justify-center mt-[40px]">
+          <div className="p-2">
+            <CustomButton
+              text="SHARE"
+              showIcon={true}
+              icon={ShareIcon}
+              onClick={() => {}}
+              className="p-2 rounded !bg-[#F2F6FF] !text-[#0066FF]"
+            />
+          </div>
+          <div className="p-2">
+            <CustomButton
+              text="COPY"
+              showIcon={true}
+              icon={CopyIcon}
+              onClick={() => {}}
+              className="p-2 rounded !bg-[#F2F6FF] !text-[#0066FF]"
+            />
+          </div>
         </div>
       </div>
+      <BottomLayout callApi={() => {}} />
     </div>
   );
 };
