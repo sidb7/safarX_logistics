41c1a1c275df9c4104683970fd4172a775895ae7
tanmay <tanmay.mungekar@shipyaari.com>
2023-08-23 18:25:58 +0530

    initial stage add product

---
+++ src/screens/NewOrder/NewCatalogue/ProductCatalogue/addProduct.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IAddProductProps {}
+
+const AddProduct: React.FunctionComponent<IAddProductProps> = (props) => {
+  return <div className="mx-4">hello there</div>;
+};
+
+export default AddProduct;
