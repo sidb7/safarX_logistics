5765ee783be9ab20b0faad96634f9dc7b445e60a
Rakesh-kariya <rakesh.kariya@shipyaari.com>
2023-09-06 17:37:23 +0530

    css-minor-fix

--- src/screens/NewOrder/Summary/pricingDetails.tsx
+++ src/screens/NewOrder/Summary/pricingDetails.tsx
@@ -35,9 +35,9 @@ const PricingDetails: React.FunctionComponent<PricingData> = ({
   const orderPrice = baseValue + addValue + variablesValue + codValue;
   console.log("orderPrice", orderPrice);
   return (
-    <div className="p-[24px]  rounded-lg border-[1px] shadow-lg border-[#E8E8E8] bg-[#F2F6FF] lg:w-[338px] lg:h-[539px] ">
+    <div className="p-[24px]  rounded-lg border-[1px] shadow-lg border-[#E8E8E8] bg-[#F2F6FF] lg:w-[338px] lg:h-[505px] ">
       <div className="flex flex-col ">
-        <div className="flex flex-col gap-y-4 mt-2  ">
+        <div className="flex flex-col gap-y-4  ">
           <div className="flex justify-between">
             <p className=" text-[12px] font-normal font-Open   lg:text-[16px] ">
               Billable Weight:
@@ -90,9 +90,9 @@ const PricingDetails: React.FunctionComponent<PricingData> = ({
             </p>
             <p> {gst} %</p>
           </div>
-          <hr className="mt-[150px]"></hr>
-          <div className="flex justify-between ">
-            <p className=" text-[12px] font-medium font-Open text-[#004EFF]  lg:text-[16px] lg:font-semibold">
+          <hr className="mt-[100px]"></hr>
+          <div className="flex justify-between mt-[5px] ">
+            <p className=" text-[12px] font-medium font-Open text-[#004EFF]  lg:text-[16px] lg:font-semibold ">
               Total:
             </p>
             <p className="text-[#004EFF]">
