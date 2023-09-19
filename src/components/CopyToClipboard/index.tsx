5adb78c3d250df8e153f8a5790ff313f13b108d6
keval-shipyaari <keval.panchal@shipyaari.com>
2023-09-19 10:54:03 +0530

    fixes

--- src/components/CopyToClipboard/index.tsx
+++ src/components/CopyToClipboard/index.tsx
@@ -1,43 +1,45 @@
 import React, { useState } from "react";
-import CopyIcon from "../../assets/OrderDetails/CopyIcon.svg";
-import "react-tooltip/dist/react-tooltip.css";
-import { Tooltip } from "react-tooltip";
+import Copy from "../../assets/copy.svg";
 interface CopyTooltipProps {
   stringToBeCopied: string;
 }
 
 const CopyTooltip: React.FC<CopyTooltipProps> = ({ stringToBeCopied }) => {
-  const [isCopied, setIsCopied] = useState(false);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const [isCopied, setIsCopied] = useState<any>(false);
+
+  const showTooltip = () => {
+    setIsTooltipVisible(true);
+  };
+
+  const hideTooltip = () => {
+    setIsTooltipVisible(false);
+  };
 
   const copyToClipboard = () => {
-    console.log("copying");
-    setIsCopied(true);
-
     navigator.clipboard.writeText(stringToBeCopied);
-
+    setIsCopied(true);
     setTimeout(() => {
       setIsCopied(false);
     }, 2000);
   };
 
   return (
-    <div className="relative inline-block w-6 mx-2 ">
+    <div className="relative inline-block mx-2">
       <img
-        src={CopyIcon}
+        src={Copy}
         alt=""
-        className="cursor-pointer "
-        data-tooltip-id="my-tooltip-inline"
-        data-tooltip-content={`${
-          isCopied ? "Copied to Clipboard ✅" : "Copy to Clipboard"
-        }`}
+        className="cursor-pointer"
+        onMouseOver={showTooltip}
+        onMouseOut={hideTooltip}
         onClick={copyToClipboard}
       />
 
-      <Tooltip
-        id="my-tooltip-inline"
-        className={`text-base bg-white text-[black]`}
-        style={{ borderRadius: "10px" }}
-      />
+      {isTooltipVisible && (
+        <div className="bg-gray-800 text-white text-xs rounded-md px-2 py-1 absolute bottom-full left-1/2 transform -translate-x-1/2 transition-opacity duration-300 opacity-100 pointer-events-none">
+          {`${isCopied ? "Copied to Clipboard" : "Copy to Clipboard"}`}
+        </div>
+      )}
     </div>
   );
 };
