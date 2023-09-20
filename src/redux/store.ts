b047fcfab061dc320c7516cb2f5d57a795a2c56f
akshaym-shipyaari <akshay.mehara@shipyaari.com>
2023-09-20 18:00:13 +0530

    bug fixed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,25 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./reducers/userReducer";
 import { PaymentSlice } from "./reducers/paymentReducer";
-import {roleSlice} from "./reducers/role"
+import { roleSlice } from "./reducers/role";
 import { mapSlice } from "./reducers/mapReducer";
 import { SignupSlice } from "./reducers/signUpReducer";
 import { SignInSlice } from "./reducers/signInReducer";
 import { catalogueSlice } from "./reducers/catalogue";
 import { onboardingSlice } from "./reducers/onboarding";
+import { Enviornment } from "../utils/ApiUrls";
 
 export const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     payment: PaymentSlice.reducer,
     map: mapSlice.reducer,
-    roles:roleSlice.reducer,
+    roles: roleSlice.reducer,
     signup: SignupSlice.reducer,
     signin: SignInSlice.reducer,
     catalogue: catalogueSlice.reducer,
     onboarding: onboardingSlice.reducer,
   },
-  devTools: true,
+  devTools: Enviornment !== "production" ? true : false,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
