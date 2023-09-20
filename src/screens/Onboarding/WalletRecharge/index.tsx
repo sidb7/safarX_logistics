dde4da5cb4d44cc547db1ba62281ad4c504913e5
tanmay <tanmay.mungekar@shipyaari.com>
2023-09-20 15:35:27 +0530

    changes updated

--- src/screens/Onboarding/WalletRecharge/index.tsx
+++ src/screens/Onboarding/WalletRecharge/index.tsx
@@ -109,7 +109,7 @@ const OnBoundingWalletRecharge = () => {
     const datas = await POST(POST_ADD_BANK_DETAILS, payload);
 
     if (datas?.data?.success) {
-      toast.success(datas?.data?.message);
+      toast.success("Bank Details verified successfully");
       setWalletRechargeModalOpen(true);
     } else {
       toast.error(datas?.data?.message);
