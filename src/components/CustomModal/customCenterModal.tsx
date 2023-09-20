325714ed4ddad60144b02bc3e3ea265d4a5ef502
keval-shipyaari <keval.panchal@shipyaari.com>
2023-09-20 17:54:25 +0530

    temp login/ signup changes

--- src/components/CustomModal/customCenterModal.tsx
+++ src/components/CustomModal/customCenterModal.tsx
@@ -21,7 +21,7 @@ const CenterModal: React.FC<CenterModalProps> = ({
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className={`${className} fixed inset-0 m-auto w-3/6 flex flex-col justify-center items-center rounded-lg overflow-hidden bg-white h-5/6`}
+      className={`${className} outline-none  fixed inset-0 m-auto w-3/6 flex active flex-col justify-center items-center rounded-lg overflow-hidden bg-white h-5/6`}
       overlayClassName="fixed z-20 inset-0 bg-gray-800 bg-opacity-50"
       shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
       contentLabel={contentLabel || "Modal"}
