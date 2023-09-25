aa926f7875ed6e24f6f56a26ca89bcf57f3ef924
keval-shipyaari <keval.panchal@shipyaari.com>
2023-09-25 19:24:31 +0530

    right side modal tranistion

--- src/components/CustomModal/customRightModal.tsx
+++ src/components/CustomModal/customRightModal.tsx
@@ -1,5 +1,5 @@
 import ReactModal from "react-modal";
-
+import "./modalStyle.css";
 interface RightSideModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -19,8 +19,9 @@ const RightSideModal: React.FC<RightSideModalProps> = ({
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onClose}
-      className={`${className} fixed outline-none inset-y-0 right-0  bg-white w-1/3  ${wrapperClassName}`}
-      overlayClassName="fixed z-20 inset-0 bg-gray-800 bg-opacity-50"
+      closeTimeoutMS={200}
+      className={`${className} fixed outline-none inset-y-0 right-0 bg-white w-1/3  ${wrapperClassName}`}
+      overlayClassName="fixed z-20 inset-0 "
     >
       {children}
     </ReactModal>
