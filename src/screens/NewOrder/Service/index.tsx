b440aef0095b1ffa618987159d782821776741d0
Rakesh-kariya <rakesh.kariya@shipyaari.com>
2023-10-04 19:09:14 +0530

    dervice-page

--- src/screens/NewOrder/Service/index.tsx
+++ src/screens/NewOrder/Service/index.tsx
@@ -227,10 +227,10 @@ const Index: React.FC = () => {
       if (response?.success) {
         setResponse(response);
 
-        let options = response?.data.map((service: any) => {
+        let options = response?.data.map((service: any, index: number) => {
           return {
             text: service,
-            value: service?.companyServiceId,
+            value: index,
           };
         });
 
@@ -258,16 +258,12 @@ const Index: React.FC = () => {
       return;
     }
 
-    let tempPayalod = response.data?.filter(
-      (service: any) => service?.companyServiceId === selectedService
-    );
-
     let {
       partnerServiceId,
       partnerServiceName,
       companyServiceId,
       companyServiceName,
-    } = tempPayalod[0];
+    } = response.data[selectedService];
 
     const payload = {
       partnerServiceId,
@@ -275,6 +271,7 @@ const Index: React.FC = () => {
       companyServiceId,
       companyServiceName,
     };
+
     try {
       const { data: response } = await POST(SET_PARTNER_SERVICE_INFO, payload);
 
