7d731df1b7531820f453411521cb98ad60dd6171
tanmay <tanmay.mungekar@shipyaari.com>
2023-11-09 17:20:06 +0530

    wallet bug

--- src/screens/Order/Payment/index.tsx
+++ src/screens/Order/Payment/index.tsx
@@ -419,12 +419,12 @@ const Payment = () => {
         <div className="mx-5 ">
           <div className="grid lg:grid-cols-2 gap-x-[27px]">
             <div className="w-full  my-5 p-3 rounded-lg border-2 border-solid border-[#E8E8E8] shadow-sm h-[200px]">
-              <div className="flex items-center gap-2 text-[1.125rem] font-semibold">
+              <div className="flex items-center gap-2 text-[#1C1C1C] font-Lato text-lg font-semibold leading-6 capitalize">
                 <img src={Accountlogo} alt="" />
                 <p className="text-[#1C1C1C]">Your wallet balance</p>
                 <p className="text-[#1C1C1C]">₹ {currentWalletValue}</p>
               </div>
-              <p className="text-[0.75rem] leading-4 text-[#BBBBBB] my-1 lg:font-normal">
+              <p className="text-[0.75rem] font-Open leading-4 text-[#BBBBBB] my-3 lg:font-normal">
                 Endless wallet balance with automatic add money
               </p>
               <CustomDropDown
