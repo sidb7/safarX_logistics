85e87eb95cef1204c257cf28a3eb3f0a269126aa
Rakesh-kariya <rakesh.kariya@shipyaari.com>
2023-11-20 18:57:51 +0530

    minor-fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,26 @@ import { useDispatch } from "react-redux";
 import { setWalletBalance } from "./redux/reducers/userReducer";
 import { socketCallbacks } from "./Socket";
 const App = () => {
-  // const [roomName, setRoomName] = useState<any>(
-  //   `${sessionStorage.getItem("sellerId")}`
-  // );
-  // const dispatch = useDispatch();
-  // const [isSocketInitialized, setIsSocketInitialized] = useState(false);
-  // console.log("isSocketconnectedApp.tsx", isSocketInitialized);
-  // useEffect(() => {
-  //   const connectSocket = async () => {
-  //     if (roomName) {
-  //       await socketCallbacks.connectSocket(dispatch);
-  //       setIsSocketInitialized(true);
-  //     }
-  //   };
+  const [roomName, setRoomName] = useState<any>(
+    `${sessionStorage.getItem("sellerId")}`
+  );
+  const dispatch = useDispatch();
+  const [isSocketInitialized, setIsSocketInitialized] = useState(false);
+  console.log("isSocketconnectedApp.tsx", isSocketInitialized);
+  useEffect(() => {
+    const connectSocket = async () => {
+      if (roomName) {
+        await socketCallbacks.connectSocket(dispatch);
+        setIsSocketInitialized(true);
+      }
+    };
 
-  //   connectSocket();
+    connectSocket();
 
-  //   return () => {
-  //     socketCallbacks.disconnectSocket();
-  //   };
-  // }, [dispatch]);
+    return () => {
+      socketCallbacks.disconnectSocket();
+    };
+  }, [dispatch]);
 
   return (
     <div>
