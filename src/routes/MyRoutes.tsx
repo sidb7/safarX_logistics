63a8f08e740f991db8cc045742165963a2f8e2fa
raja <raja.chauhan@shipyaari.com>
2023-11-23 15:32:04 +0530

    testing

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -154,11 +154,15 @@ import BankProtected from "../components/ProtectedRoutes/BankProtected";
 import PublicTracking from "../screens/PublicScreens/PublicTracking";
 import InvoicePdf from "../screens/Invoice";
 import CashOnDelivery from "../screens/Onboarding/WalletRecharge/cashOnDelivery";
+import AiAddress from "../screens/QuickPages/AiAddress";
+import AiListen from "../screens/QuickPages/AiListen";
 
 const MyRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/ai-address" element={<AiAddress />} />
+        <Route path="/ai-listen" element={<AiListen />} />
         <Route path="/auth/login" element={<LogInScreen />} />
         <Route
           path="/"
@@ -1244,6 +1248,8 @@ const MyRoutes: React.FC = () => {
           />
 
           <Route path="*" element={<NotFound />} />
+
+
         </Route>
       </Routes>
     </BrowserRouter>
