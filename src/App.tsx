044a4402b6f0fcdd09e0050dc7da42d1778e040b
keval-shipyaari <keval.panchal@shipyaari.com>
2023-12-05 19:19:15 +0530

    Google Tag Manager init

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,33 @@ import { useStore } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setWalletBalance } from "./redux/reducers/userReducer";
 import { socketCallbacks } from "./Socket";
+import TagManager from "react-gtm-module";
+
+declare global {
+  interface Window {
+    dataLayer?: any;
+  }
+}
+
 const App = () => {
+  const tagManagerArgs: any = {
+    gtmId: process.env.REACT_APP_GTM_ID,
+  };
+
   const [roomName, setRoomName] = useState<any>(
     `${sessionStorage.getItem("sellerId")}`
   );
   const dispatch = useDispatch();
   const [isSocketInitialized, setIsSocketInitialized] = useState(false);
   console.log("isSocketconnectedApp.tsx", isSocketInitialized);
+
   useEffect(() => {
+    //Init G Tag Manager
+    TagManager.initialize(tagManagerArgs);
+
+    console.log("window", window);
+
+    //Socket Connectionu
     const connectSocket1 = async () => {
       if (roomName) {
         await socketCallbacks.connectSocket(dispatch);
