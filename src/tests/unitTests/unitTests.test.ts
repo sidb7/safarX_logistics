eb9391b90bdbcbd68a8872c5ec4e5f26c2e47483
Rakesh-kariya <rakesh.kariya@shipyaari.com>
2023-12-06 15:15:20 +0530

    file-upload-tests-added

--- src/tests/unitTests/unitTests.test.ts
+++ src/tests/unitTests/unitTests.test.ts
@@ -1,5 +1,9 @@
-import { date_DD_MMM, date_DD_MMM_YYY, date_DD_MMM_YYYY_HH_MM, date_DD_MMM_YYYY_HH_MM_SS } from "../../utils/dateFormater";
+// @ts-nocheck
 
+import { date_DD_MMM, date_DD_MMM_YYY, date_DD_MMM_YYYY_HH_MM, date_DD_MMM_YYYY_HH_MM_SS } from "../../utils/dateFormater";
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
+import { convertXMLToXLSX } from "../../utils/helper";
 describe('Date formatting functions', () => {
   const mockTimestamp = 1701855494000; 
 
@@ -41,5 +45,68 @@ describe('Date formatting functions', () => {
     const locale = 'en-IN';
     const expected = new Intl.DateTimeFormat(locale, options)?.format(new Date(mockTimestamp));
     expect(formattedDate).toBe(expected);
+  });
+});
+
+
+
+
+jest.mock('xlsx', () => ({
+  utils: {
+    json_to_sheet: jest.fn(),
+    book_new: jest.fn(),
+    book_append_sheet: jest.fn(),
+  },
+  write: jest.fn(),
+}));
+
+jest.mock('file-saver', () => ({
+  saveAs: jest.fn(),
+}));
+
+describe('convertXMLToXLSX function', () => {
+  const apiData = [{ name: 'John', age: 30 }, { name: 'Jane', age: 25 }];
+  const filename = 'example.xlsx';
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('convertXMLToXLSX successfully converts and saves the file', async () => {
+    // Mock XLSX functions
+    XLSX.utils.json_to_sheet.mockReturnValue('mockedWorksheet');
+    XLSX.utils.book_new.mockReturnValue('mockedWorkbook');
+    XLSX.write.mockResolvedValue('mockedBlob');
+
+    // Call the function
+    const result = await convertXMLToXLSX(apiData, filename);
+
+    // Assertions
+    expect(result).toBe(true);
+    expect(XLSX.utils.json_to_sheet).toHaveBeenCalledWith(apiData);
+    expect(XLSX.utils.book_new).toHaveBeenCalled();
+    expect(XLSX.utils.book_append_sheet).toHaveBeenCalledWith('mockedWorkbook', 'mockedWorksheet', 'Sheet 1');
+    expect(XLSX.write).toHaveBeenCalledWith('mockedWorkbook', { bookType: 'xlsx', type: 'array' });
+    expect(FileSaver.saveAs).toHaveBeenCalledWith(
+      new Blob(['mockedBlob'], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8',
+      }),
+      filename
+    );
+  });
+
+  test('convertXMLToXLSX handles errors and returns false', async () => {
+    // Mock XLSX functions to simulate an error
+    XLSX.utils.json_to_sheet.mockImplementation(() => {
+      throw new Error('Mocked error');
+    });
+
+    // Call the function
+    const result = await convertXMLToXLSX(apiData, filename);
+
+    // Assertions
+    expect(result).toBe(false);
+    expect(XLSX.utils.json_to_sheet).toHaveBeenCalledWith(apiData);
+    // Add more assertions if needed for error handling
   });
 });
