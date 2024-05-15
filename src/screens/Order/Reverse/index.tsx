0bbbd3ad37c98f291a2223d852496cf3421d82a6
sureshp-shipyaari <suresh.parmar@shipyaari.com>
2024-05-15 14:51:59 +0530

    start working on reverse

---
+++ src/screens/Order/Reverse/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReverDummyJson from "./reverseDummy.json";
+
+const index = () => {
+  console.log("ReverDummyJson", ReverDummyJson);
+
+  return <div>index</div>;
+};
+
+export default index;
