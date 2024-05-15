1e9615aec93831fb6084f78d54e5ec1eaecb93bc
sureshp-shipyaari <suresh.parmar@shipyaari.com>
2024-05-15 16:48:06 +0530

    add new radio btn for reverse and forward

--- src/screens/Order/Reverse/index.tsx
+++ src/screens/Order/Reverse/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReverDummyJson from "./reverseDummy.json";
 
+// ${
+//   isActiveItemAccordionOpen && activeItem === roleData?.id
+//     ? "bg-[#F6F6F6] rounded-none rounded-t-lg"
+//     : "bg-white"
+// }
 const index = () => {
   console.log("ReverDummyJson", ReverDummyJson);
 
-  return <div>index</div>;
+  return (
+    <div className="accordionContainerBoxStyle">
+      <div
+        className={`cursor-pointer px-4 py-3 flex justify-between items-center  bg-[#F6F6F6] rounded-none rounded-t-lg`}
+        // onClick={() => toggleAccordion(roleData?.id)}
+        // key={index}
+      ></div>
+    </div>
+  );
 };
 
 export default index;
