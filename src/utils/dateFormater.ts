46f563639a91c668bf4863470e3f73be6c237665
Rakesh-kariya <rakesh.kariya@shipyaari.com>
2024-08-23 17:54:59 +0530

    updated-at-format-fixed

--- src/utils/dateFormater.ts
+++ src/utils/dateFormater.ts
@@ -9,7 +9,26 @@ export const date_DD_MMM_YYY = (epochTimestamp: number) => {
 export const date_DD_MMM_YYYY_HH_MM = (epochTimestamp: number) => {
   const date = new Date(epochTimestamp);
   const dateOptions: any = { year: "numeric", month: "short", day: "numeric" };
-  const timeOptions: any = { hour: "2-digit", minute: "2-digit" };
+  const timeOptions: any = {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Asia/Kolkata",
+  };
+
+  const formattedDate = date.toLocaleDateString("en-IN", dateOptions);
+  const formattedTime = date.toLocaleTimeString("en-IN", timeOptions);
+
+  return `${formattedDate}, ${formattedTime}`;
+};
+
+export const date_DD_MMM_YYYY_HH_MM_IST = (epochTimestamp: number) => {
+  const date = new Date(epochTimestamp);
+  const dateOptions: any = { year: "numeric", month: "short", day: "numeric" };
+  const timeOptions: any = {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Asia/Kolkata",
+  };
 
   const formattedDate = date.toLocaleDateString("en-IN", dateOptions);
   const formattedTime = date.toLocaleTimeString("en-IN", timeOptions);
@@ -20,7 +39,11 @@ export const date_DD_MMM_YYYY_HH_MM = (epochTimestamp: number) => {
 export const date_DD_MMM_YYYY_HH_MM_SS = (epochTimestamp: number) => {
   const date = new Date(epochTimestamp);
   const dateOptions: any = { year: "numeric", month: "short", day: "numeric" };
-  const timeOptions: any = { hour: "2-digit", minute: "2-digit", second: "2-digit" };
+  const timeOptions: any = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
   const formattedDate = date.toLocaleDateString("en-IN", dateOptions);
   const formattedTime = date.toLocaleTimeString("en-IN", timeOptions);
