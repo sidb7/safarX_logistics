c44b12bf15dc0561b7c3dabc4d41195d298efbd9
sureshp-shipyaari <suresh.parmar@shipyaari.com>
2024-09-04 16:34:28 +0530

    changes socket message

--- src/components/GlobalToast/GlobalToast.tsx
+++ src/components/GlobalToast/GlobalToast.tsx
@@ -1,7 +1,11 @@
 import { toast } from "react-hot-toast";
 
 export const GlobalToast = (message: string): void => {
-  toast.error(message);
+  if (message === "Order in processing") {
+    toast.success(message);
+  } else {
+    toast.error(message);
+  }
 };
 
 export const GlobalToastSuccess = (message: string): void => {
