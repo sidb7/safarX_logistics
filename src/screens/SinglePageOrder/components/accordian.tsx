2985d935258b5265fec08a72361be0a711f0836a
Amitr-s <amit.mishra@shipyaari.com>
2024-09-05 16:44:34 +0530

    refactor

--- src/screens/SinglePageOrder/components/accordian.tsx
+++ src/screens/SinglePageOrder/components/accordian.tsx
@@ -11,15 +11,20 @@ function Accordian({
 }: any) {
   const [accordianOpen, setAccordianOpen] = useState(false);
   const isLgScreen = useMediaQuery({ query: "(min-width: 1024px)" });
+
+  console.log("accordian-comdition", !accordianOpen && !isLgScreen);
+
   return (
     <div className={`${className} items-center scroll-smooth w-[100%]`}>
       {(!isLgScreen || showHeaderForDesktop) && (
         <div
           className={`flex rounded-tl-lg rounded-tr-lg ${
-            accordianOpen ? "border-b" : "rounded-bl-lg rounded-br-lg"
+            accordianOpen && "border-b"
           } items-center z-10  top-0  p-2 ${
             !isLgScreen ? "bg-[#F6F6F6] " : "bg-[#ffffff] border-b"
-          } bg-[#F6F6F6] top-0`}
+          } bg-[#F6F6F6] top-0 
+          ${!accordianOpen && !isLgScreen ? "rounded-bl-lg rounded-br-lg" : ""}
+          `}
           style={{ position: "sticky" }}
         >
           {headerChild}
