26769f36f4a1f2bce8a14dbf32f31e8e8f238bc3
Keval <keval.panchal@shipyaari.com>
2024-11-28 12:14:52 +0530

    boot logo changes

--- src/screens/BootScreen/index.tsx
+++ src/screens/BootScreen/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import CompanyLogo from "../../assets/CompanyLogo/shipyaari icon.svg";
+import { LARGE_LOGO } from "../../utils/ApiUrls";
 
 const Index = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ const Index = () => {
     <div className="flex items-center justify-center h-screen">
       <img
         className="animate-bounce object-contain"
-        src={CompanyLogo}
+        src={LARGE_LOGO}
         alt="bootscreen"
       />
     </div>
