50a77eba2fbafaba23c141b06b1d4bad03d20649
sureshp-shipyaari <140158163+sureshp-shipyaari@users.noreply.github.com>
2024-12-03 16:26:08 +0530

    Non editable single page (#2056)
    * disabled the invoice value and collectable value after place order in single order
    
    * disabled the invoice value and collectable value after place order in single order
    
    * valid email on signup

--- src/screens/Onboarding/Signup/index.tsx
+++ src/screens/Onboarding/Signup/index.tsx
@@ -67,16 +67,16 @@ const Index = () => {
   });
 
   const signUpOnClick = async (value: any) => {
-    // if (
-    //   signUpError.email ||
-    //   signUpError.firstName ||
-    //   signUpError.lastName ||
-    //   signUpError.password ||
-    //   signUpError.referalCode
-    // ) {
-    //   toast.error("Please complete all the fields.");
-    //   return;
-    // }
+    if (
+      signUpError.email ||
+      signUpError.firstName ||
+      signUpError.lastName ||
+      signUpError.password ||
+      signUpError.referalCode
+    ) {
+      toast.error("Please resolved all the fields.");
+      return;
+    }
 
     try {
       let signupUtm = {
@@ -410,14 +410,21 @@ const Index = () => {
                       //commented as by default placeholder text is getting top of the input box
                       // tempLabel={true}
                       onChange={(e) => {
-                        setSignUpError({
-                          ...signUpError,
-                          email: "",
-                        });
-                        setsellerData({
-                          ...sellerData,
-                          email: e.target.value,
-                        });
+                        if (emailRegex.test(e.target.value)) {
+                          setSignUpError({
+                            ...signUpError,
+                            email: "",
+                          });
+                          setsellerData({
+                            ...sellerData,
+                            email: e.target.value,
+                          });
+                        } else {
+                          setSignUpError({
+                            ...signUpError,
+                            email: "Enter Valid Email",
+                          });
+                        }
                       }}
                       onBlur={(e) => {
                         if (!sellerData?.email) {
