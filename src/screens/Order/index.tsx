e5d6fa65295060111f668bf13a6e666c662c25c5
Suresh Parmar <biswajeet@gmail.com>
2024-12-12 21:57:44 +0530

    fix the sync channel

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -36,6 +36,7 @@ import {
   PAYMENT_ERRORS,
   DUPLICATE_ORDER,
   COMPANY_NAME,
+  GET_COUNT_AMAZON_ORDER,
 } from "../../utils/ApiUrls";
 import OrderCard from "./OrderCard";
 import "../../styles/index.css";
@@ -416,6 +417,10 @@ const Index = () => {
   });
   const [isBulkCheckedBooked, setIsBulkCheckedBooked]: any = useState(false);
 
+
+  const [fullfillment, setFullfillment] = useState();
+  const [unFullfillment, setUnFullfillment] = useState();
+
   //  // Add ref for the abort controller
   //    const [renderingComponents, setRenderingComponents] = useState<number>(0);
 
@@ -760,6 +765,15 @@ const Index = () => {
           ) : (
             <></>
           )}
+
+          <div className="flex gap-3">
+            <span className="flex flex-1 min-w-fit items-center py-[6px] rounded-md px-[10px] border-[1px] border-[#A4A4A4]   font-medium text-[#1C1C1C]">
+              Amazon Fullfilment ({fullfillment})
+            </span>
+            <span className="flex flex-1 min-w-fit items-center py-[6px] rounded-md px-[10px] border-[1px] cursor-pointer border-[#A4A4A4]   font-medium text-[#1C1C1C]">
+              Amazon Unfullfilment ({unFullfillment})
+            </span>
+          </div>
 
           {isModalOpen && (
             <CenterModal
@@ -1949,6 +1963,21 @@ const Index = () => {
       );
     }
   }, [endDate, activeTab, searchedText]);
+
+  useEffect(() => {
+  (async () => {
+    try {
+      const { data } = await POST(GET_COUNT_AMAZON_ORDER);
+      if(data?.code === 200){
+        let temp = data?.data?.[0];
+        setFullfillment(temp?.fullFillMent?.[0]?.fullFillMent || 0);
+        setUnFullfillment(temp?.unfullFillMent?.[0]?.unfullFillCount || 0);
+      }
+    } catch (error) {
+      toast.error("Something went wrong")
+    }
+  })();
+}, []);
 
   const onPageIndexChange = async (data: any) => {
     let skip: any = 0;
