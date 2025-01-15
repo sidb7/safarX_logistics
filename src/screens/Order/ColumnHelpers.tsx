c547f598517c4c2dd95333e5713488779452f5bb
kumashsy <kumash.shah@shipyaari.com>
2025-01-15 14:35:45 +0530

    Code change for buyer verification logic

--- src/screens/Order/ColumnHelpers.tsx
+++ src/screens/Order/ColumnHelpers.tsx
@@ -1242,20 +1242,21 @@ export const columnHelperForNewOrder = (
                     ? date_DD_MMM_YYYY_HH_MM_SS(createdAt)
                     : time}
                 </div>
+
                 <div>
-                  {buyerConfirmation === "UNVERIFIED" ? (
+                  {buyerConfirmation === "BUYER CANCELLED" ? (
                     <p className="px-2 py-2 w-[150px] bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
-                      BUYER CANCELLED
+                      {buyerConfirmation}
                     </p>
                   ) : buyerConfirmation === "PENDING" ? (
-                    <p className="px-2 py-2 w-[130px] bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
-                      PENDING
+                    <p className="px-2 py-2 w-[80px] bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+                      {buyerConfirmation}
                     </p>
-                  ) : (
+                  ) : buyerConfirmation === "BUYER VERIFIED" ? (
                     <span className="px-2 py-2 w-[150px] bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                      BUYER VERIFIED
+                      {buyerConfirmation}
                     </span>
-                  )}
+                  ) : null}
                 </div>
               </div>
             }
