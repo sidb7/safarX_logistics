36c089ca59ed4110fb56f254ce781e3912b61fda
piyush <piyush.gharde@shipyaari.com>
2025-04-03 16:22:42 +0530

    not added to fall back

--- src/screens/NewOrder/Tracking/LostAndDamaged/LostAndDamaged.tsx
+++ src/screens/NewOrder/Tracking/LostAndDamaged/LostAndDamaged.tsx
@@ -644,7 +644,7 @@ const LostAndDamaged: React.FC = () => {
             <p className="mt-4 text-gray-600">Loading shipment data...</p>
           </div>
         </div>
-      ) : fallback ? (
+      ) : !fallback ? (
         <div className="flex flex-col items-center justify-center h-[calc(100vh-300px)]">
           <img src={centerIcon} alt="Lost and Damaged Icon" className="mb-4" />
 
