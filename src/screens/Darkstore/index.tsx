cea2b202f6502c7e6beb735d19312fdeffe1e669
sumedhindulkar <sumedh.indulkar@shreemaruti.com>
2025-05-21 17:34:20 +0530

    name change

--- src/screens/Darkstore/index.tsx
+++ src/screens/Darkstore/index.tsx
@@ -43,21 +43,19 @@ function DarkstoreInventory() {
           <Breadcrum label="Dark Store Inventory" />
           <div className="flex flex-col md:flex-row gap-4 px-[20px]">
             <div className="w-full md:w-1/2 border border-gray-200 shadow-md rounded-lg px-4 py-6">
-              <h2 className="text-lg mb-2">Total all Darkstores</h2>
+              <h2 className="text-lg mb-2">Total Products</h2>
               <p className="text-xl font-bold mb-1">
                 {totalProducts.product_name}
               </p>
-              <p className="text-md text-gray-600">Across all darkstores</p>
+              <p className="text-md text-gray-600">Across all dark stores</p>
             </div>
 
             <div className="w-full md:w-1/2 border border-gray-200 shadow-md rounded-lg px-4 py-6">
-              <h2 className="text-lg font-semibold mb-2">Charges</h2>
+              <h2 className="text-lg font-semibold mb-2">Dark stores</h2>
               <p className="text-lg font-bold mb-1">
                 ₹ {totalProducts.dark_store}
               </p>
-              <p className="text-sm text-gray-600">
-                Along with all other darkstore
-              </p>
+              <p className="text-sm text-gray-600">Active Locations</p>
             </div>
           </div>
           <div className="px-[20px]">
